4ffadbfa6895bed727ca07f2d55bcaa62feb926f
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-03-23 19:46:32 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,8 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif 
