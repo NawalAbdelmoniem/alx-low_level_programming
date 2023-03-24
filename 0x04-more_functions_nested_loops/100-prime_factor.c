273bd76050a129607636e0621aa48707d6ef1197
NawalAbdelmoniem <125440893+NawalAbdelmoniem@users.noreply.github.com>
2023-03-24 23:14:41 +0200

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,83 @@
 #include <stdio.h>
+#include "holberton.h"
 /**
- * main - entry point
+ * main - prints the biggest prime factor of a number.
  *
- * Return: always 0 (sucess)
+ * Return: Always 0.
  */
 int main(void)
 {
-unsigned long int num = 612852475143;
-unsigned long int prim;
-prim = 3;
-while (prim < num / 2)
-{
-if ((num % prim) == 0)
-{
-if ((prim % 3) == 2)
-printf(",%lu ", prim);
+	long int number;
+
+	number = 612852475143;
+
+	if (isPrime(number) == 1)
+	{
+		printf("%ld\n", number);
+	}
+	else
+	{
+		printf("%ld\n", biggestFactor(number));
+	}
+	return (0);
 }
-prim + = 2;
+/**
+ * isPrime - analise if a number is prime or not
+ * @n: number to check
+ * Return: true if it is prime false if not
+ */
+int isPrime(long int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		for (i = 2; i < n; i++)
+		{
+			if (n % i == 0)
+			{
+				return (0);
+			}
+		}
+	return (1);
+	}
 }
-putchar('\n');
-return (0);
+/**
+ * biggestFactor - returns the biggest prime factor of a number
+ * @a: number to check
+ * Return: biggest factor
+ */
+long int biggestFactor(long int a)
+{
+	long int i, factor;
+
+	factor = a;
+	for (i = 2; i <= factor; i++)
+	{
+		if (isPrime(factor) == 1)
+		{
+			break;
+		}
+		else
+		{
+			if ((factor % i == 0) && (isPrime(i) == 1))
+			{
+				factor = factor / i;
+				continue;
+			}
+			else
+			{
+				factor = factor;
+			}
+		}
+	}
+	return (factor);
 }
