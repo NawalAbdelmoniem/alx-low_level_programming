e964fc47fff941b3ddd943517e2db94e53a6bb7a
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-03-27 14:36:57 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ *Write a function that swaps the values of two integers
+ *int *a, int *b
+ *Return: Always 0.
+*/
+void swap_int(int *a, int *b);
+}
+int c;
+c = *a;
+*a = *b;
+*b = c;
+return (0);
+}
