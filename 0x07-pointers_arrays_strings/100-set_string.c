542304fba04369a394d297dddeb5dc1e01859583
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-03 16:19:15 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*set_string : set a value to a pointer to char
+* @s : pointer to pointer need to be set
+* @to : string to be set
+*/
+
+void set_string(char **s, char *to);
+{
+s = to;
+}
