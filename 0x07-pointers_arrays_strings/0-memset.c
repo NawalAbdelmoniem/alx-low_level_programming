4c8c4a58a53162d20b56c21242c4e510ef79e104
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-03 16:39:08 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,19 +3,18 @@
 /**
 * *_memset - fills memory with a constant byte
 * @s : pointer to memory
-* @b : the desired constant byte
+* @b : the desired constant byte Char copy
 * @n : number of times
 *
-* Return : pointer to memory areas s
+* Return : pointer to memory area (s)
 */
 
 char *_memset(char *s, char b, unsigned int n);
 {
-int i;
+unsinged int i;
 for (i = 0; i < n; i++)
 {
 S[i] = b;
-n--;
 }
 return (s);
 }
