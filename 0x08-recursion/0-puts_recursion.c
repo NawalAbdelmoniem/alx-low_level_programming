141b2df3bc7542741441570522f951b532a7e479
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-08 15:06:03 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 void _puts_recursion(char *s);
 int main(void)
 {
-int *s;
+char  *s;
 if (*s == '\0')
 {
 _putchar (*s);
-_puts_recursion(s + 1);
+_puts_recursion(*s + 1);
 }
 else
 _putchar('\n');
