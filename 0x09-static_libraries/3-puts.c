18564ff2fcb4c466fd99bb2d852eb968973ea7e1
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-09 15:12:32 -0700

    static_libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,10 @@
 #include "main.h"
-/**                                                                                                                         
- *_puts - prints a string                                                                                                   
- * @str: string to print                                                                                                    
- * Description: prints a string                                                                                             
- * On success: return the number of characters printed                                                                      
+#include <stdio.h>
+/**                                                                                                            
+ *_puts - prints a string                                                                                                  
+ * @str: string to print                                                                                                   
+ * Description: prints a string                                                                                            
+ * On success: return the number of characters printed                                                                     
  */
 void _puts(char *str)
 {
@@ -11,11 +12,10 @@ while (*str)
 _putchar(*str++);
 _putchar('\n');
 }
-#include <stdio.h>
-/**                                                                                                                         
- * main - Entry point                                                                                                       
- *                                                                                                                          
- * Return: Always 0 (Success)                                                                                               
+/**                                                                                                                        
+ * main - Entry point                                                                                                      
+ *                                                                                                                         
+ * Return: Always 0 (Success)                                                                 
  */
 int main(void)
 {
