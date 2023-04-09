e3daf8b18af066919a4b024c7041b28f5a66258d
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-08 17:03:19 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,7 @@
-/**
- *Author: Nawal
- *task : 1-print_rev_recursion.c
- */
 #include "main.h"
 /**
  *main - check the code
- *_print_rev_recursion: print a string in reverse
+ *_print_rev_recursion:function that prints a string in reverse
  * @s: print string
  *return : no return
  */
