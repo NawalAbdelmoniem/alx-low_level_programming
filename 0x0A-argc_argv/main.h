68814c858f4213796498e3cac7ad646ca90a083a
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-10 15:35:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+/**
+ *Author : Nawal Abdelmoniem
+ *Main.h : contains all the prototypes for all functions
+ */
+int _putchar(char c);
+int atoi(const char *str);
+#endif
