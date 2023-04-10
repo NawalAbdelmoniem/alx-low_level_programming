c223ab372268471fd2907bafd0dd259f4ba09573
NawalAbdelmoniem <125440893+NawalAbdelmoniem@users.noreply.github.com>
2023-04-10 22:58:23 +0200

    3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -12,13 +12,3 @@ while (*str)
 _putchar(*str++);
 _putchar('\n');
 }
-/**                                                                                                                        
- * main - Entry point                                                                                                      
- *                                                                                                                         
- * Return: Always 0 (Success)                                                                 
- */
-int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}
