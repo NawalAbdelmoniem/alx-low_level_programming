4a6d0f46388542b289b984c85af49af30c253f80
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-13 15:40:07 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+/**
+ *Author : Nawal Abdelmoniem
+ *Main.h : contains all the prototypes for all functions
+ */
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
