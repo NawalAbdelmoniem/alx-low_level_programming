7e8750272c47c25bce536aef24321324dcbffe36
Nawal Abdelmoniem <nawal.mohamed0@gmail.com>
2023-04-17 16:33:23 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints the name of the file
- * ain : starting point for program execution
+ * main : starting point for program execution
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
