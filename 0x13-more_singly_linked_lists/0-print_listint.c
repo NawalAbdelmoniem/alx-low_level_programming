569f86f0ed78e76b7cae90feb6aa3d974537d144
NawalAbdelmoniem <Nawal.mohamed0@gmail.com>
2023-05-01 13:22:29 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,12 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t n = 0;
+size_t numb = 0;
 while (h)
 {
 printf("%d\n", h->n);
-n++;
+numb++;
 h = h->next;
 }
-return (n);
+return (numb);
 }
