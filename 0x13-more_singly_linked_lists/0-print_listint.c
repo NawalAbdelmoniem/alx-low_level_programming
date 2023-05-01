ca737156119bc1be91f9786c6314124ca628b8f9
NawalAbdelmoniem <Nawal.mohamed0@gmail.com>
2023-05-01 14:07:19 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,16 +2,16 @@
 /**
  * print_listint - prints linked list
  * @h: linked list of type listint_t to print
- * Return: number of nodes
+ * Return: number_of_nodes
  */
 size_t print_listint(const listint_t *h)
 {
-size_t number = 0;
+size_t number_of_nodes = 0;
 while (h)
 {
 printf("%d\n", h->n);
-number++;
+number_of_nodes++;
 h = h->next;
 }
-return (number);
+return (number_of_nodes);
 }
